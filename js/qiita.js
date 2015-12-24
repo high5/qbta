44353694a0ac4f2f1cfe4319f54a9e1d8f884dd6
high5 <high525jp@gmail.com>
2015-12-24 18:23:32 +0900

    Delete extra space

--- js/qiita.js
+++ js/qiita.js
@@ -9,7 +9,6 @@ function patch(){
     subwindow = window.open('http://b.hatena.ne.jp/add?mode=confirm&is_bm=1&title='+escape(document.title) + '&tags=' + tagsStr + '&url=' + escape(location.href), '_blank', 'width=520,height=600,resizable=1,scrollbars=1');undefined;
   });
 }
-
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('('+patch+')();'));
 document.body.appendChild(script);
